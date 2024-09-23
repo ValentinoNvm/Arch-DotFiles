83fe64d3d1f0181aff64a46cd0d4b2da76e82d1f
Valentino_Nvm <33992517+ValentinoNvm@users.noreply.github.com>
2024-09-23 19:30:36 +0000

    Update script.sh

--- i3/script.sh
+++ i3/script.sh
@@ -1,7 +1,5 @@
 !#/bin/bash
-
-#wallpaper
-feh --bg-scale /home/ferrodastiro/Downloads/wallpaper.png
+#!/bin/bash
 
 #force resolution on primary monitor
 xrandr --output DP-0 --mode 2560x1440
@@ -12,14 +10,22 @@ xrandr --output DP-0 --right-of HDMI-0
 #polybar execution
 ~/.config/polybar/launch.sh
 
+sleep 5
+
 #picom config file (opacity)
-picom  --config ~/.config/picom/picom.conf
+picom  --config ~/.config/picom/picom.conf &
+
+sleep 5
 
 #nvidia fans
 sudo nvidia-settings -a '[gpu:0]/GPUFanControlState=1' -a '[fan:0]/GPUTargetFanSpeed=50'
 
+sleep 5
+
 #flameshot startup
-/usr/bin/flameshot
+/usr/bin/flameshot &
+
+sleep 5
 
 #remove standby etc
 xset s off
